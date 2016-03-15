4ebcb1b2a9465bfabbb07a29f0ed3b1d2e574ac8
notteen <notteen@gmail.com>
2016-03-15 22:31:44 +0700

    new logo

--- SourceCode/Tunga/src/java/utility/DataProcess.java
+++ SourceCode/Tunga/src/java/utility/DataProcess.java
@@ -5,9 +5,7 @@
  */
 package utility;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
